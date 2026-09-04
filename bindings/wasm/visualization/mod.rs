541525a8dd715a2b87a14c68c15b8e8237f4e6fb
Jojain <romain.ferru@gmail.com>
2026-09-04 16:50:55 +0200

    removed old boolean script

--- bindings/wasm/visualization/mod.rs
+++ bindings/wasm/visualization/mod.rs
@@ -2,6 +2,6 @@ mod scripts;
 mod tcv;
 
 pub use scripts::{
-    boolean_configuration, extrude_polygon, list_scripts, revolve_triangle, run_script,
+    extrude_polygon, list_scripts, revolve_triangle, run_script,
 };
 pub use tcv::{block_scene, hydrate_debug_geometry, scene_from_gmap};
