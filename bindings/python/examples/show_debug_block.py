51bcb8497ffd7e0aa2cd09730bab523959f2aaee
Jojain <romain.ferru@gmail.com>
2026-09-16 17:42:23 +0200

    added plan targetting build123d backend

--- bindings/python/examples/show_debug_block.py
+++ bindings/python/examples/show_debug_block.py
@@ -4,9 +4,14 @@ p = Path(r"D:\Projets\ngk\tests\exchange\foreign\files")
 
 files = p.glob("*.step")
 
+
+
 solid = ngk.block(1.0, 2.0, 3.0)
+solid = ngk.cylinder(1.0, 2.0)
+# ngk.debug.show(solid, name="solid")
 for file in files:
     print(file)
     step = ngk.exchange.step.read_step(file)
     c = step.solids[0]
     ngk.debug.show(c, name=file.stem)
+        # break
